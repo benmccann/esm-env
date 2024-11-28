ee2b6387f01545dea72c131621e8c4b8de4593df
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-11-28 12:10:19 -0800

    warn instead of error

--- packages/esm-env/dev-fallback.js
+++ packages/esm-env/dev-fallback.js
@@ -1,6 +1,6 @@
 const node_env = globalThis.process?.env?.NODE_ENV;
 if (!node_env) {
-	throw new Error('conditions should include development or production. See https://www.npmjs.com/package/esm-env for tips on setting conditions in popular bundlers and runtimes.');
+	console.warn('If bundling, conditions should include development or production. If not bundling, conditions or NODE_ENV should include development or production. See https://www.npmjs.com/package/esm-env for tips on setting conditions in popular bundlers and runtimes.');
 }
 
-export default node_env !== 'production';
+export default node_env && !node_env.toLowerCase().includes('prod');
