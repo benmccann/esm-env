0c000789ae5cb4ff46a20de89bf98b56e490bd43
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-11-28 16:21:32 -0800

    fix: improve fallbacks (#13)

--- packages/esm-env/dev-fallback.js
+++ packages/esm-env/dev-fallback.js
@@ -1,13 +1,6 @@
-if (!import.meta.env) {
-	// changes to import.meta only apply to this file
-	// we have to do this to be able to directly access import.meta.env.DEV
-	// bunlder defines such as Vite will only replace import.meta.env.DEV and not import.meta.env?.DEV
-	// we also can't do import.meta.env && import.meta.env.DEV because that breaks tree-shaking
-	import.meta.env = {};
+const node_env = globalThis.process?.env?.NODE_ENV;
+if (!node_env) {
+	console.warn('If bundling, conditions should include development or production. If not bundling, conditions or NODE_ENV should include development or production. See https://www.npmjs.com/package/esm-env for tips on setting conditions in popular bundlers and runtimes.');
 }
 
-// import.meta.env.DEV can be replaced by a bundler define such as Vite does and the rest of this
-// expression can then be condensed down to a single boolean by the bundler if such a define is used
-// Otherwise, if there were no conditions defined and the build time replacement of import.meta.env.DEV
-// didn't happen then fallback to runtime check of process.env.NODE_ENV
-export default !!(import.meta.env.DEV ?? process?.env?.NODE_ENV !== 'production');
+export default node_env && !node_env.toLowerCase().includes('prod');
